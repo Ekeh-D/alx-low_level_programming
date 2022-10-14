31ae8301d26fc1801a747fba8d3835f7da949a1d
Ekeh-D <ekeh4567@gmail.com>
2022-10-14 14:42:11 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,18 +3,23 @@
 #include <stdio.h>
 
 /**
-* main - prints "Programming is like building a multilingual puzzle
-* @void: Function argument
-* Return: 0 if the code runs successfuly. 1 otherwise
+*main -> assign a random number
+*based a condition
+*Return: always 0
 */
-
-/* betty style doc for functionmain goes there */
 int main(void)
 {
 int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
+if (n > 0)
+	printf("%d is positive\n", n);
+if (n == 0)
+	printf("%d is zero\n", n);
+if (n < 0)
+{
+	printf("%d is negative\n", n);
+}
 return (0);
 }
