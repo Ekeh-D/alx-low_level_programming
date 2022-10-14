20e2edfd0ead76c67babc16fb58741acf507b416
Ekeh-D <ekeh4567@gmail.com>
2022-10-14 12:35:53 -0700

    0. Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - prints "Programming is like building a multilingual puzzle
+* @void: Function argument
+* Return: 0 if the code runs successfuly. 1 otherwise
+*/
+
+/* betty style doc for functionmain goes there */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+return (0);
+}
